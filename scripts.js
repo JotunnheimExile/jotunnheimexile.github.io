0654566039318cab45e552b57373005c415fd681
Jötunn <hvrtache@gmail.com>
2025-04-29 09:58:47 +0300

    Update scripts.js
    29.04.25 - Added a script for "reveal".

--- scripts.js
+++ scripts.js
@@ -1,4 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const reveals = document.querySelectorAll(".reveal");
+
+    const options = {
+        threshhold: 0.1
+    };
+
+    const observer = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("active");
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    reveals.forEach(reveal => {
+        observer.observe(reveal);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
     const slides = document.querySelectorAll('.slide');
     if (slides.length > 0) {
         let currentSlide = 0;
@@ -9,10 +30,10 @@ document.addEventListener("DOMContentLoaded", function() {
         }
 
         function nextSlide() {
-            currentSlide = (currentSlide + 1) % slides.length;
+            currentSlide = (currentSlide + 1) % slides.length; // loop back to 0
             showSlide(currentSlide);
         }
 
-        setInterval(nextSlide, 4000);
+        setInterval(nextSlide, 4000); // slide every 4 seconds
     }
 });
